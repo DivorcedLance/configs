faf01eb9c9a87f69891cf49a1de52e01e1acf9e1
DivorcedLance <divorcedlance@gmail.com>
2024-04-28 19:29:14 -0500

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+oh-my-posh init pwsh --config 'C:\Users\Jose\AppData\Local\Programs\oh-my-posh\themes\tiwahu.omp.json' | Invoke-Expression
+Import-Module -Name Terminal-Icons
+Set-pSReadLineoption -PredictionSource History
+Set-PSReadLineoption -Predictionviewstyle Listview
+Set-PSReadLineOption -EditMode windows
+. C:\Users\Jose\Documents\PowerShell\gh-copilot.ps1
